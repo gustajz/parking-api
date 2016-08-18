0dadab1343f75133b50b7e54ca2be33bf4eb7eb6
Gustavo Jotz <gustavo@jotz.com.br>
2016-08-18 00:37:02 -0300

    travis docker

--- docker_push.sh
+++ docker_push.sh
@@ -7,8 +7,8 @@ export VERSION=`xml2 < pom.xml  | grep /project/version= | sed 's/.*=//'`
 echo docker tag $REPO gustajz/$REPO
 docker tag $REPO gustajz/$REPO
 
-echo docker tag $REPO gustajz/$VERSION
-docker tag $REPO gustajz/$VERSION
+echo docker tag $REPO gustajz/$REPO:$VERSION
+docker tag $REPO gustajz/$REPO:$VERSION
 
 echo docker push gustajz/$REPO:latest
 docker push gustajz/$REPO:latest
