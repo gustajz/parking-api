a1fb6f9aadaf00de97e77acfef03992e4f879165
Gustavo Jotz <gustavo@jotz.com.br>
2016-08-23 00:25:01 -0300

    Configuração para testes

---
+++ src/test/java/parking/repository/test/ProprietarioRepositoryTest.java
@@ -0,0 +1,38 @@
+package parking.repository.test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import parking.domain.Proprietario;
+import parking.repository.ProprietarioRepository;
+
+/**
+ * 
+ * @author gustavojotz
+ *
+ */
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase
+@Sql("classpath:data-test.sql")
+public class ProprietarioRepositoryTest {
+
+	@Autowired
+	private ProprietarioRepository repository;
+
+	@Test
+	public void findByUsuarioRetornaProprietario() {
+		Proprietario p = this.repository.findByUsuario("gustavojotz");
+
+		assertThat(p.getNome()).isEqualTo("Gustavo Jotz");
+		assertThat(p.getUsuario()).isEqualTo("gustavojotz");
+	}
+
+}
