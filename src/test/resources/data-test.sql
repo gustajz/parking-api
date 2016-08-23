767609d578cc011394c0008fdd3f60bb40b2e013
Gustavo Jotz <gustavo@jotz.com.br>
2016-08-23 00:25:16 -0300

    Carga de proprietario

---
+++ src/test/resources/data-test.sql
@@ -0,0 +1,5 @@
+INSERT INTO proprietario(
+            id, usuario, nome, celular, ramal, andar, posicao, gravatar, 
+            dh_criacao, dh_login)
+    VALUES (1, 'gustavojotz', 'Gustavo Jotz', '555198769876', 640, 4, 'P111', 'a1234as', 
+            '2016-08-01 00:00:00', '2016-08-01 01:00:00');
